9fbc4bf20bdec92e8e59326aa00e46a51a401ead
Danny Wu <wushuo1988@hotmail.com>
2014-09-09 22:34:21 -0400

    Fixed bug and typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
 
-  def role?(bsse_model)
+  def role?(base_role)
     role == base_role.to_s
   end
 end
