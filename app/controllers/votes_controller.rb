1781c422a31e69d4ad23a5aff5c10a4e9ddd0f8d
Danny Wu <wushuo1988@hotmail.com>
2014-09-18 21:23:21 -0400

    Fixed up_vote method in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,19 +2,11 @@ class VotesController < ApplicationController
   before_action :load_post_and_vote
 
   def up_vote
+    update_vote(1)
     redirect_to :back
   end
 
-  def update_vote(new_value)
-    if @vote
-       authorize @vote, :update?
-      @vote.update_attribute(:value, new_value)
-    else
-       @vote = current_user.votes.build(value: new_value, post: @post)
-       authorize @vote, :create?
-       @vote.save
-    end
-  end
+
 
 
   private
@@ -24,15 +16,15 @@ class VotesController < ApplicationController
     @vote = @post.votes.where(user_id: current_user.id).first
   end
 
-  def update_vote!(new_value)
+  def update_vote(new_value)
     if @vote
-      @vote.update_attribute(:value, 1)
+      authorize @vote, :update?
+      @vote.update_attribute(:value, new_value)
     else
-      @vote = current_user.votes.create(value: 1, post: @post)
+       @vote = current_user.votes.build(value: new_value, post: @post)
+       authorize @vote, :create?
+       @vote.save
     end
   end
-
-
-
 
 end
