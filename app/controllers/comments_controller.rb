fa0ac5ab2fbdc3951ec1ea466e369e14541c913e
Danny Wu <wushuo1988@hotmail.com>
2014-09-28 22:31:36 -0400

    Implemented Ajax for comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
 
   def create
     @post = Post.find(params[:post_id])
@@ -25,11 +26,13 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
     end
+
+      respond_with(@comment) do |format|
+        format.html { redirect_to [@post.topic, @post]}
+      end
   end
 
   private
